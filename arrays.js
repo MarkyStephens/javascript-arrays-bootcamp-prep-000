08eb2bb00f0cfab5888c9f1928d9eb5ae916e08c
Mark Stephens <mstephens@trackeleven.com>
2018-01-24 22:04:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,5 +9,6 @@ function addElementToBeginningOfArray(array, element) {
 return [element, ...array]
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  
+  array.unshift(element)
+  return 
 }
