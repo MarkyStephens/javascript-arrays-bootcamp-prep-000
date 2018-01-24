ece6cbd1f732ef7fe9f5cbcb29b301f025ae26a1
Mark Stephens <mstephens@trackeleven.com>
2018-01-24 22:24:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -13,7 +13,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   return array
 }
 function addElementToEndOfArray(array, element){
-  [...array, element]
+  return [...array, element]
 }
 function destructivelyAddElementToEndOfArray(array, element){
   array.push(element)
